3504c42078e8299fb4d41020f8fffc52e3674917
oket23 <oleggolub460@gmail.com>
2025-06-26 20:40:03 +0300

    123

---
+++ LearningLanguageApp/LearningLanguageApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LearningLanguageApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!123");
+        }
+    }
+}
